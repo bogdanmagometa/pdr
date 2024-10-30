f2d5547b09882aa3594830b967513b3ca550dab8
Bohdan <bogdan.magometa@gmail.com>
2024-10-30 19:58:19 +0200

    launch

--- launch.sh
+++ launch.sh
@@ -5,5 +5,5 @@ uvicorn main:app --port 8000 --host localhost &
 popd
 
 pushd frontend
-PDR_MS_HOSTNAME=localhost PDR_MS_PORT=8000 streamlit run main.py &
+PDR_MS_HOSTNAME=localhost PDR_MS_PORT=8000 streamlit run --server.address localhost --server.port 8080 main.py &
 popd
